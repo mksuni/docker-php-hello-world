9688bbf4999314b2b0460ed21bb66d2ea83cce6d
Sunitha Muthukrishna <sumuth@microsoft.com>
2020-11-17 01:32:01 -0800

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,7 +3,7 @@
 $conn = mysqli_init();
 $conn->options(MYSQLI_OPT_SSL_VERIFY_SERVER_CERT, true);
 $conn->ssl_set($conn, NULL, "/var/www/html/DigiCertGlobalRootCA.crt.pem", NULL, NULL);
-$conn->real_connect( getenv('DBHOST'), getenv('USER'), getenv('PASSWORD'),'flexibleserverdb', 3306, MYSQLI_CLIENT_SSL);
+$conn->real_connect( getenv('DBHOST'), getenv('USER'), getenv('PASSWORD'),'flexibleserverdb');
 if (mysqli_connect_errno($conn)) {
 die('Failed to connect to MySQL: '.mysqli_connect_error());
 }
